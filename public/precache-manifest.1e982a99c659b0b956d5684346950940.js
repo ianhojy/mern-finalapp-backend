fcb614a5bf83a565d187bc266515a45d0fe32284
Ian Ho <ianhojy@gmail.com>
2023-03-19 23:37:58 +0800

    added frontend react

---
+++ public/precache-manifest.1e982a99c659b0b956d5684346950940.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae988068b8e2555c5e980df7e7c3818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39cefbc2281a36ee3d0d",
+    "url": "/static/css/0.3df8cb10.chunk.css"
+  },
+  {
+    "revision": "a929de7ffdf634d77202",
+    "url": "/static/css/4.475d84a6.chunk.css"
+  },
+  {
+    "revision": "bda49aeef688dee514eb",
+    "url": "/static/css/5.1326459c.chunk.css"
+  },
+  {
+    "revision": "f3f03c9ab83861da7a2b",
+    "url": "/static/css/6.295134cd.chunk.css"
+  },
+  {
+    "revision": "c67f9c6056c5db151ef5",
+    "url": "/static/css/7.9a0d14f5.chunk.css"
+  },
+  {
+    "revision": "f444a37a3b109036d5be",
+    "url": "/static/css/8.c540964c.chunk.css"
+  },
+  {
+    "revision": "1fad05f1e408a390bb1d",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "39cefbc2281a36ee3d0d",
+    "url": "/static/js/0.16ecea5f.chunk.js"
+  },
+  {
+    "revision": "388c6c4bd7d1cc6387ed",
+    "url": "/static/js/3.cb262acf.chunk.js"
+  },
+  {
+    "revision": "a929de7ffdf634d77202",
+    "url": "/static/js/4.33319548.chunk.js"
+  },
+  {
+    "revision": "bda49aeef688dee514eb",
+    "url": "/static/js/5.401c74cf.chunk.js"
+  },
+  {
+    "revision": "f3f03c9ab83861da7a2b",
+    "url": "/static/js/6.1a53e001.chunk.js"
+  },
+  {
+    "revision": "c67f9c6056c5db151ef5",
+    "url": "/static/js/7.4d0db4ab.chunk.js"
+  },
+  {
+    "revision": "f444a37a3b109036d5be",
+    "url": "/static/js/8.9b3caca5.chunk.js"
+  },
+  {
+    "revision": "1fad05f1e408a390bb1d",
+    "url": "/static/js/main.94fdeb95.chunk.js"
+  },
+  {
+    "revision": "fd4ece6961618171d86d",
+    "url": "/static/js/runtime-main.9d0e80f6.js"
+  }
+]);
